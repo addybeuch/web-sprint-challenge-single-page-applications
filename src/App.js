0e58c2cb47a4ebf0fed4bd2c047b67abf5cb387f
addybeuch <addybeuch@gmail.com>
2021-09-17 16:50:54 -0600

    Finished form layout and testing appjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { Link , Route, Switch } from 'react-router-dom'
+import Pizza from "./_tests_/Pizza"
+import schema from './_tests_/formSchema'
+import * as yup from 'yup';
+import { string } from "yup/lib/locale";
+
+
+
+const initialFormValues = {
+  name: '',
+  size: '',
+  pepperoni: false,
+  sausage: false,
+  bacon: false,
+  chicken: false,
+  special: '',
+}
+
+const initialFormErrors = {
+  name: '',
+  size: '',
+  special: '',
+}
+
+const initialPizzas = []
+const initialDisabled = true
 
 const App = () => {
+
+const [pizzas, setPizzas] = useState(initialPizzas)
+const [formValues, setFormValues] = useState(initialFormValues)
+const [formErrors, setFormErrors] = useState(initialFormErrors)
+const [disabled, useDisabled] = useState(initialDisabled)
+
+const postPizza = newPizza => {
+setPizzas([newPizza, ...pizzas])
+setFormValues(initialFormValues);
+}
+
+const validate = (name, value) => {
+  yup.reach(schema, name)
+    .validate(value)
+    .then(() => setFormErrors({...formErrors, [name]:''}))
+    .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]}))
+}
+
+const inputChange = (name, value) => {
+  validate(name, value);
+  setFormValues({
+    ...formValues,
+    [name]: value
+  })
+}
+
+const formSubmit = () => {
+  const newPizza = {
+    name: formValues.name.trim(),
+    size: formValues.size,
+    pepperoni: formValues.pepperoni,
+    sausage: formValues.sausage,
+    bacon: formValues.bacon,
+    chicken: formValues.chicken,
+    special: formValues.special.trim()
+
+  }
+  postPizza(newPizza);
+}
+
+
+
+
   return (
     <>
     <div className='app'>
-      <h1>Lambda Eats</h1>
+    <Link to='/'>Home</Link>
       <Switch>
         <Route exact path={'/'}>
         <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/Pizza'>Pizza Maker</Link>
+            <h1>Lambda Eats</h1>
+            <p>Mmmm beenza!</p>
+            <Link id='order-pizza' to='/Pizza'>Order Pizza</Link>
         </nav>
         </Route>
-        <Route path={'Pizza'}>
-          <Pizza/>
+        <Route path={'/Pizza'}>
+          <Pizza
+          values={formValues} 
+          change={inputChange}
+          submit={formSubmit}
+          disabled={disabled}
+           />
           </Route>
       </Switch>
 
-      <p>You can remove this code and create your own header</p>
+
     </div>
     </>
   );
--- src/_tests_/Pizza.js
+++ src/_tests_/Pizza.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function pizza(props) {
+export default function Pizza(props) {
     const {values, submit, change, disabled} = props
 
     const onSubmit = evt => {
@@ -14,18 +14,66 @@ export default function pizza(props) {
     }
 
     return (
-    <form className='form-container' onSubmit={onSubmit}>
+    <form className='form-container' id='pizza-form' onSubmit={onSubmit}>
         <h1>Pizza Maker</h1> 
         <div className='pizza-form'>
             <label>Name
                 <input 
-                    value={values.name}
-                    onChange={onChange}
                     name='name'
                     type='text'
+                    value={values.name}
+                    onChange={onChange}
                 />
-                    
-            </label>                 
+            </label>
+            <label>Size 
+                <select name="size">
+                    <option value='option1'>Small</option>                 
+                    <option value='option2'>Medium</option>
+                    <option value='option3'>Large</option>
+                    <option value='option4'>X-Large</option>
+                </select>
+            </label>
+            <label>Pepperoni
+                <input
+                   name='pepperoni'
+                   type='checkbox'
+                   checked={values.pepperoni}
+                   onChange={onChange}
+                   />
+            </label>
+            <label>Sausage
+                <input
+                   name='sausage'
+                   type='checkbox'
+                   checked={values.sausage}
+                   onChange={onChange} 
+                   />
+            </label>
+            <label>Bacon
+                <input
+                   name='bacon'
+                   type='checkbox'
+                   checked={values.bacon}
+                   onChange={onChange} 
+                   />
+            </label>
+            <label>Chicken
+                <input
+                   name='chicken'
+                   type='checkbox'
+                   checked={values.chicken}
+                   onChange={onChange} 
+                   />
+            </label>
+            <label>Special Instructions
+                <input 
+                name='special'
+                type='text'
+                value={values.special}
+                onChange={onChange}
+                />
+            </label>
+            <button id='order-button' disabled={disabled}>Submit</button>
         </div>
     </form>
     )
